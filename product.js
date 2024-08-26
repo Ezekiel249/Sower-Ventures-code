21a17f3355a7cba32933a9fe010ed4d73fead9cb
Ezekiel <adelowopeoluwa249@gmail.com>
2024-08-26 12:50:32 +0100

    updated01

---
+++ product.js
@@ -0,0 +1,18 @@
+const products = [{
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    image: 'images/FEMALE PALM.png',
+    rating:{
+        stars: 4.5,
+        count: 25,
+    },
+    priceCent: 1090,
+},
+{
+    name: 'Intermediate Size Basketball',
+    image: 'images/FEMALE PALM.png',
+    rating:{
+        star: 4.5,
+        count: 25,
+    },
+    priceCent: 2095,
+}];
